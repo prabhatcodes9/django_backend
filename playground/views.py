974b198dc7783f5601badd33a18b6539414cab8a
prabhatcodes9 <prabhatkumar1065@gmail.com>
2025-08-02 16:44:02 +0530

    query

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.contrib.contenttypes.models import ContentType
+from store.models import Product
+from tags.models import TaggedItem
 # Create your views here.
 
 def say_hello(request):
-    return render(request, 'hello.html', { 'name': 'King'})
+    # This use to query sql
+    queryset = TaggedItem.objects.get_tags_for(Product, 1)
+
+    return render(request, 'hello.html', { 'name': 'King', 'result': list(queryset)})
