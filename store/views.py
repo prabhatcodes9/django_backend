b0ca29cf38251e4319611a2b708e4d07da519916
prabhatcodes9 <prabhatkumar1065@gmail.com>
2025-08-04 20:12:04 +0530

    searching_sorting

--- store/views.py
+++ store/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import get_object_or_404
-from django.http import HttpResponse
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework import status
 from .models import Product, Collection, Review
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
@@ -17,8 +14,10 @@ from django.db.models import Count
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['collection_id']
+    search_fields = ['title']
+    ordering_fields = ['unit_price']
     
     
     def get_serializer_context(self):
