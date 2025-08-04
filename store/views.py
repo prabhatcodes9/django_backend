3b5311bfeb1d4f35d6d0945d2b32c81f84f44e4a
prabhatcodes9 <prabhatkumar1065@gmail.com>
2025-08-04 20:21:46 +0530

    add_pagination

--- store/views.py
+++ store/views.py
@@ -4,6 +4,7 @@ from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework import status
+from rest_framework.pagination import PageNumberPagination
 from .models import Product, Collection, Review
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
 from django.db.models import Count
@@ -16,6 +17,7 @@ class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['collection_id']
+    pagination_class = PageNumberPagination
     search_fields = ['title']
     ordering_fields = ['unit_price']
     
